71d602980bae7e57fd3d94f81d0c4c148fe02f92
YigitDilibal <archery10@outlook.com>
2024-12-01 21:17:39 +0200

    v0.2

--- src/P01.java
+++ src/P01.java
@@ -5,6 +5,20 @@ public class P01 {
 
         System.out.println("Hello Git-Github");
 
+        /*
+
+        ilk olusturulup github ile baglanildiginda
+        github tarafindan verilen kodlar uygulanir
+
+        sonraki gonderimlerde
+        git add .
+        git commit -m "message" <- bu asamada git uzerinde versiyonlama tamamlanmistir bir sonraki komut sadece githuba gondermek icindir.
+        git push
+
+        mesajlari kullanilir.
+
+         */
+
 
     }
 }
