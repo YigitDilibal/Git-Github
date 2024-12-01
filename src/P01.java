65e6e923fa595eca21cfc233cde660f308fa30cb
YigitDilibal <archery10@outlook.com>
2024-12-01 21:13:34 +0200

    v0.1

--- src/P01.java
+++ src/P01.java
@@ -3,7 +3,7 @@ public class P01 {
     public static void main(String[] args) {
 
 
-        System.out.println("Merhaba Git-Github");
+        System.out.println("Hello Git-Github");
 
 
     }
---
+++ src/P02.java
@@ -0,0 +1,7 @@
+public class P02 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
+}
